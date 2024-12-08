00db5e1b5523c20bd4ed79d8a43efa88c57151cd
SomewhatMay <umayeer.ahsan@hotmail.com>
2024-12-08 15:27:33 -0500

    refactored to use scope resolution operator more often

--- src/file-input.cpp
+++ src/file-input.cpp
@@ -4,17 +4,16 @@
 
 #include "globals.h"
 
-using std::vector, std::string;
 namespace fs = std::filesystem;
 
-vector<string> get_question_list() {
+std::vector<std::string> get_question_list() {
   // Change this line to use an absolute or relative path as needed
-  vector<string> question_list;
+  std::vector<std::string> question_list;
   fs::directory_iterator iterator(globals::QUESTIONS_PATH);
 
   for (const auto &entry : iterator) {
     if (entry.is_directory()) {
-      string question_id = entry.path().filename().string();
+      std::string question_id = entry.path().filename().string();
       question_list.push_back(question_id);
     }
   }
--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,11 @@
 #include "file-input.h"
 #include "globals.h"
 
-using std::cout, std::string, std::vector;
-
 int main() {
-  vector<string> question_list = get_question_list();
-  cout << "Questions: \n";
+  std::vector<std::string> question_list = get_question_list();
+  std::cout << "Questions: \n";
   for (const auto &question_id : question_list) {
-    cout << question_id << '\n';
+    std::cout << question_id << '\n';
   }
   return 0;
 }
