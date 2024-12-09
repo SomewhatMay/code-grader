3e4670e4387b9cbd0eb509b4eed83731f149ab23
SomewhatMay <umayeer.ahsan@hotmail.com>
2024-12-09 18:13:45 -0500

    under work - get test cases from question_id

--- include/file-input.h
+++ include/file-input.h
@@ -4,6 +4,28 @@
 #include <vector>
 
 /**
- * Return a vector of names based on the folders in resources/questions.
+ * Contain the format of a test case as defined
+ * in the question files.
+ */
+struct test_case {
+  std::string title;
+  std::string description;
+  std::vector<std::string> inputs;
+  std::vector<std::string> outputs;
+};
+
+/**
+ * Return a vector of `question_id`s based on the names
+ * of each question in the resources/questions folder.
  */
 std::vector<std::string> get_question_list();
+
+/**
+ * Return a vector of test cases in the question with name
+ * `question_id`.
+ *
+ * This function does not throw. If the test case file is malformed
+ * and cannot be parsed, it will return an empty vector and output
+ * the .what() in the stdout.
+ */
+std::vector<test_case> get_test_cases(std::string question_id);
--- src/file-input.cpp
+++ src/file-input.cpp
@@ -1,6 +1,8 @@
 #include "file-input.h"
 
 #include <filesystem>
+#include <fstream>
+#include <iostream>
 
 #include "globals.h"
 
@@ -19,4 +21,29 @@ std::vector<std::string> get_question_list() {
   }
 
   return question_list;
+}
+
+enum parse_status {
+  SEARCHING_FOR_TITLE,
+  IN_TITLE,  // Either looking for description or inputs
+  INPUT_FOUND,
+  OUTPUT_FOUND,
+};
+
+std::vector<test_case> get_test_cases(std::string question_id) {
+  std::vector<test_case> test_cases;
+  std::ifstream question_file(std::string(globals::QUESTIONS_PATH) +
+                              question_id);
+
+  if (!question_file) {
+    std::cerr << "Unable to open file with path " << question_id;
+    return test_cases;
+  }
+
+  std::string line;
+  parse_status status = SEARCHING_FOR_TITLE;
+  while (std::getline(question_file, line)) {
+    if (SEARCHING_FOR_TITLE) {
+        }
+  }
 }
